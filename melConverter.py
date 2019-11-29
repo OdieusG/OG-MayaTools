3aee75644892232f7052f10c3306681223cd4627
OdieusG <odiusg@gmail.com>
2019-11-29 13:51:10 -0500

    Update melConverter.py

--- melConverter.py
+++ melConverter.py
@@ -1,5 +1,13 @@
 '''
 Converter for MELScript to Pyton via window
+
+
+Usage:
+
+import OG_MayaTools.melConverter as melConvert
+reload(melConvert)
+melConvert.gui()
+
 '''
 import pymel.core as pm
 import string
@@ -8,3 +16,6 @@ import pymel.tools.mel2py as mel2py
 def gui():
     conversionWindow = pm.window(title="Converter Window")
     pm.columnLayout()
+    pm.text(label="MEL Script To Convert:")
+    melText = pm.textField(placeholderText="Insert Text Here")
+
