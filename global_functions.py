7794dadebae30176e269606b0e799ed887bd8b66
OdieusG <odiusg@gmail.com>
2019-12-25 22:31:48 -0500

    Update global_functions.py

--- global_functions.py
+++ global_functions.py
@@ -4,7 +4,6 @@ These can be used globally within any files
 '''
 
 print "Global functions executed"
-'''def snap_to_joint(targetObject, objectToSnap):
+def snap_to_joint(targetObject, objectToSnap):
 	# Sends the object to the pivot point of the second argument
 	# Get the pivot point of 
-'''
