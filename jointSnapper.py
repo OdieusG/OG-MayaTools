6381232dbb1114ac00c544d7252e66c6bb3ccca2
OdieusG <odiusg@gmail.com>
2019-12-26 09:19:00 -0500

    Update jointSnapper.py

---
+++ jointSnapper.py
@@ -0,0 +1,18 @@
+'''
+Joint snapper
+1.0
+
+Snaps pads and controls to joint that alre already established
+
+How to run:
+
+import OG_MayaTools.jointSnapper as js
+reload(js)
+js.gui()
+'''
+
+
+
+
+def gui():
+	print "Output GUI"
