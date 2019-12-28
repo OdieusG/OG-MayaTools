8932298584f6c6f981870ddd7cb0b963d7e1ad84
OdieusG <odiusg@gmail.com>
2019-12-28 01:08:05 -0500

    Update jointSnapper.py

--- jointSnapper.py
+++ jointSnapper.py
@@ -11,8 +11,31 @@ reload(js)
 js.gui()
 '''
 
+import pymel.core as pm
+import maya.cmds as cmds
+import string
 
 
 
 def gui():
-	print "Output GUI"
+	snapperWindow = pm.window()
+	pm.columnLayout()
+	pm.frameLayout(collapsable=True, label='Create padding')
+	pm.button(label='Create pad on joint')
+	pm.button(label='Pad control and joint')
+	pm.button(label='Test button')
+	pm.setParent('..')
+	pm.frameLayout(collapsable=True, label='Snap to...')
+	pm.scrollLayout(height=60)
+	pm.text(label='field')
+	pm.text(label='field')
+	pm.text(label='field')
+	pm.text(label='field')
+	pm.text(label='field')
+	pm.text(label='field')
+	pm.text(label='field')
+	pm.setParent('..')
+	pm.button()
+
+
+	snapperWindow.show()
