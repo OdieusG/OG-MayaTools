428cbfc3c14b3b31c12b61e0a0a110f593b9512a
OdieusG <odiusg@gmail.com>
2020-02-22 02:22:58 -0500

    Update connector_mod.py

--- connector_mod.py
+++ connector_mod.py
@@ -24,7 +24,7 @@ padObject = ""
 def toast(message):
     cmds.headsUpMessage(message)
 
-def getSelected(dagOption=True, niceName=False):
+def getSelected(niceName=False, dagOption=True):
 	selectedItem = cmds.ls(sl=True, dag=dagOption)
 	if niceName == True:
 		return selectedItem[0]
@@ -38,34 +38,57 @@ def closeWindow():
 def selectItem(*args):
 	# Break down the passed selected into
 	#selItem = getSelected(False, True)
-	pm.text("padObject", label=str(getSelected(True, True)), edit=True)
+	pm.text("padObject", label=str(getSelected(True)), edit=True)
 
 def padObject(*args):
+	global appendPad
 	# Get the location of the object getting padded (xform)
 	initialObject  = getSelected()
 	initialTranslate = cmds.xform(initialObject[0], q=True, t=True)
 	initialRotate = cmds.xform(initialObject[0], q=True, ro=True)
 	# Create group
-	pm.group(em=True, n="newGroup")
+	padName = str(getSelected(True))
+	if appendPad.getValue() == True:
+		padName = padName + "_pad"
+	pm.group(em=True, n=padName)
 	# Move group to location of object
-	cmds.xform("newGroup", t=initialTranslate, ro=initialRotate)
+	cmds.xform(padName, t=initialTranslate, ro=initialRotate)
 	# Move object into group
-	cmds.parent(initialObject[0], "newGroup")
+	cmds.parent(initialObject[0], padName)
 	closeWindow()
 
 def gui():
-	global connector_window
+	global connector_window, appendPad
 	connector_window = pm.window(title="Connector Window", width=windowWidth, height=windowHeight, sizeable=False)
-	pm.scrollLayout()
+	pm.tabLayout(innerMarginWidth=5, innerMarginHeight=5)
+	child1 = pm.scrollLayout("Quick Snaps")
+
 	pm.frameLayout(collapsable=True, label="Pad Object", width=(windowWidth-10))
+
 	pm.rowLayout(numberOfColumns=3, columnWidth=[(1, 150), (2, 200), (3, 150)], columnAlign=[(3, 'right')])
 	pm.text(label="Choose an object to pad:")
 	pm.text("padObject", label="<i>nothing selected</i>")
 	pm.button(label="Choose item", command=pm.Callback(selectItem))
 	pm.setParent("..")
-	pm.columnLayout()
-	pm.button(label="Pad this", command=pm.Callback(padObject))
+
+	pm.rowLayout(numberOfColumns=2, columnWidth=[(1,350)])
+	pm.text("Automatically append \"_pad\" to end?")
+	appendPad = pm.checkBox("appendPad", value=True, label="")
 	pm.setParent("..")
+
+	pm.rowLayout(numberOfColumns=3)
+	pm.text(width=150, label="")
+	pm.button(label="Pad this", command=pm.Callback(padObject))
+	pm.text(width=150, label="")
+	pm.setParent("..")
+
+	pm.scrollLayout("Other Stuff")
+	pm.text("test")
+	pm.setParent("..")
+
+	pm.setParent("..")
+
+
 	pm.checkBox("Automatically close window", value=True)
 
 	pm.showWindow(connector_window)
