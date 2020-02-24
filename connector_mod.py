25d76688a3fc4bdcdea85d6f402bb8e9d6b7201d
OdieusG <odiusg@gmail.com>
2020-02-24 16:48:53 -0500

    Update connector_mod.py

--- connector_mod.py
+++ connector_mod.py
@@ -57,19 +57,25 @@ def padObject(*args):
 	cmds.parent(initialObject[0], padName)
 	closeWindow()
 
+def createShape(*args):
+	print("Create shapes")
+
 def gui():
 	global connector_window, appendPad
 	connector_window = pm.window(title="Connector Window", width=windowWidth, height=windowHeight, sizeable=True)
-	pm.columnLayout(numberOfChildren=2)
+	pm.columnLayout(numberOfChildren=2, columnAlign="left")
 	mainTabs = pm.tabLayout(innerMarginWidth=5, innerMarginHeight=5)
 
 	tab_1 = pm.columnLayout(numberOfChildren=2)
+	# Tab1 begin
 	pm.text(label="<i>Things to work on</i>")
 	pm.text(label="Autosnapper\nJoint connector\nShape maker integration", align="left")
 	pm.setParent(mainTabs)
+	# Tab1 end
+
 
 	tab_2 = pm.scrollLayout(width=windowWidth-10, height=windowHeight-50)
-
+	#Tab 2 begin
 	pm.frameLayout(collapsable=True, label="Pad Object", width=(windowWidth-10))
 
 	pm.rowLayout(numberOfColumns=3, columnWidth=[(1, 150), (2, 200), (3, 150)], columnAlign=[(3, 'right')])
@@ -89,12 +95,47 @@ def gui():
 	pm.text(width=150, label="")
 	pm.setParent("..")
 
-	pm.frameLayout
 	pm.setParent(mainTabs)
+	#Tab 2 end
+
+	tab_3 = pm.columnLayout(numberOfChildren=2, columnAlign="left")
+	pm.frameLayout(label="Choose options:", width=450)
 	
-	tab_3 = pm.frameLayout(collapsable=True, label="Shape Maker", width=windowWidth-40)
-	pm.text(label="testing")
+	pm.rowLayout(numberOfColumns=2, columnAlign=([1, "left"], [2, "right"]), width=450, columnWidth=([1, 200], [2, 250]))
+	pm.text(label="Choose a shape:")
+	pm.optionMenu(width=100)
+	pm.menuItem(label="Circle")
+	pm.menuItem(label="Square")
+	pm.menuItem(label="Cube")
+	pm.menuItem(label="2D Arrow")
+	pm.menuItem(label="3D Arrow")
+	pm.menuItem(label="COG Circle")
+	pm.menuItem(label="Compass")
+	pm.setParent("..")
+	
+	pm.rowLayout(numberOfColumns=2, columnAlign=([1, "left"], [2, "right"]), width=450, columnWidth=([1, 200], [2, 250]))
+	pm.text(label="Enter a name.\nLeave blank for default shape name.")
+	shapeName = pm.textField(placeholderText="shape")
+	pm.setParent("..")
+
+	pm.rowLayout(numberOfColumns=2, columnAlign=([1, "left"], [2, "right"]), width=450, columnWidth=([1, 200], [2, 250]))
+	pm.text(label="Insert a suffix.\nLeave blank for default\n\"_icon\" to be added")
+	shapeSuffix = pm.textField(placeholderText="_icon")
+	pm.setParent("..")
+
+	pm.rowLayout(numberOfColumns=2, columnAlign=([1, "left"], [2, "right"]), width=450, columnWidth=([1, 200], [2, 250]))
+	pm.text(label="Shape is placed on:")
+	pm.optionMenu(width=150)
+	pm.menuItem(label="Origin")
+	pm.menuItem(label="Currently Selected Object")
+	pm.setParent("..")
+
+	pm.rowLayout(numberOfColumns=1, columnAlign=([1, "center"]), width=450, columnWidth=([1,450]))
+	pm.button(label="Create Shape", command=pm.Callback(createShape))
+	pm.setParent("..")
+
 	pm.setParent(mainTabs)
+	#Tab 3 end
 
 	pm.setParent("..")
 
@@ -102,12 +143,7 @@ def gui():
 	mainTabs.setTabLabel([tab_2, "Snapper"])
 	mainTabs.setTabLabel([tab_3, "Shape Maker"])
 
-	pm.rowLayout(numberOfColumns=2)
-	pm.text(label="Test - ")
-	pm.optionMenu(label="Colors")
-	pm.menuItem(label="1")
-	pm.menuItem(label="2")
-	pm.setParent("..")
+	
 
 	pm.checkBox("Automatically close window", value=True)
 
